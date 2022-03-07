d1279120db2a5d675c6e8d87d90c653c671b29c3
“shubham767676” <shubhamv.singh06@gmail.com>
2022-03-07 10:01:16 +0530

    Added: Welcome Message

---
+++ src/com/bridgelabz/deckofcards/DeckOfCardsMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.deckofcards;
+
+public class DeckOfCardsMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Deck of Cards"); // Displaying Welcome Message
+		DeckOfCards deckOfCardsGame = new DeckOfCards();
+		deckOfCardsGame.deckOfCard();
+
+	}
+}
